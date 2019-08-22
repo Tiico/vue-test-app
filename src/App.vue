b334e3bc5b9a9f57ae42b9f83c63705d10a6fb4f
tiico <ockelberg@gmail.com>
2019-08-22 12:43:19 +0200

    working!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your fuuuuuuunkaaaa.js App"/>
+    <HelloWorld msg="Welcome to Your react.js App"/>
   </div>
 </template>
 
