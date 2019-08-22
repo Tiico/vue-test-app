233460a02b8234bcd205be84e297bbd6de632a9f
tiico <ockelberg@gmail.com>
2019-08-22 11:22:44 +0200

    fixed port issue

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.get('/api/customers', (req, res) => {
   res.json(customers);
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "/dist/"));
 
