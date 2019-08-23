84dab55e2527a55d357d4b6738cbd62b00eae072
tiico <ockelberg@gmail.com>
2019-08-23 11:16:48 +0200

    travis

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to an actual App"/>
+    <HelloWorld msg="Welcome to the test App"/>
   </div>
 </template>
 
